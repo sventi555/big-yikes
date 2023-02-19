1644916dccdbaa244a4392a7263c5f8a2b9fea7f
Stephen Edwards <34760298+sventi555@users.noreply.github.com>
2023-02-19 18:39:31 -0500

    fix: remove default port from firestore setup

--- packages/server/src/db/index.ts
+++ packages/server/src/db/index.ts
@@ -4,6 +4,6 @@ import { toBool, toInt } from 'lib';
 export const db = new Firestore({
   projectId: process.env.FIRESTORE_PROJECT_ID || 'local',
   host: process.env.FIRESTORE_HOST || 'localhost',
-  port: toInt(process.env.FIRESTORE_PORT) || 8000,
+  port: toInt(process.env.FIRESTORE_PORT),
   ssl: toBool(process.env.FIRESTORE_SSL) || false,
 });
