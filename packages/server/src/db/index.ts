9fea281d41928d9b0c38e8d40e15da12381ae41c
Stephen Edwards <34760298+sventi555@users.noreply.github.com>
2023-02-19 22:31:05 -0500

    fix: exclude host and port from db options if undefined

--- packages/server/src/db/index.ts
+++ packages/server/src/db/index.ts
@@ -1,9 +1,17 @@
-import { Firestore } from '@google-cloud/firestore';
+import { Firestore, Settings } from '@google-cloud/firestore';
 import { toBool, toInt } from 'lib';
 
-export const db = new Firestore({
+const options: Settings = {
   projectId: process.env.FIRESTORE_PROJECT_ID || 'local',
-  host: process.env.FIRESTORE_HOST || 'localhost',
-  port: toInt(process.env.FIRESTORE_PORT),
   ssl: toBool(process.env.FIRESTORE_SSL),
-});
+};
+
+if (process.env.FIRESTORE_HOST) {
+  options.host = process.env.FIRESTORE_HOST;
+}
+
+if (process.env.FIRESTORE_PORT) {
+  options.port = toInt(process.env.FIRESTORE_PORT);
+}
+
+export const db = new Firestore(options);
