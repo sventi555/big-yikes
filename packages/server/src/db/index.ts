aed81b3127808316f8dc0df016d167c4289fa102
Stephen Edwards <34760298+sventi555@users.noreply.github.com>
2023-02-19 19:19:53 -0500

    chore: no need for default ssl

--- packages/server/src/db/index.ts
+++ packages/server/src/db/index.ts
@@ -5,5 +5,5 @@ export const db = new Firestore({
   projectId: process.env.FIRESTORE_PROJECT_ID || 'local',
   host: process.env.FIRESTORE_HOST || 'localhost',
   port: toInt(process.env.FIRESTORE_PORT),
-  ssl: toBool(process.env.FIRESTORE_SSL) || false,
+  ssl: toBool(process.env.FIRESTORE_SSL),
 });
