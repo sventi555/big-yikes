7e2e00fabde5b60b67c7071791bda925978b3c71
Stephen Edwards <34760298+sventi555@users.noreply.github.com>
2023-02-20 11:46:53 -0500

    fix: use cloud run env for db connection

--- packages/server/src/db/index.ts
+++ packages/server/src/db/index.ts
@@ -1,17 +1,15 @@
 import { Firestore, Settings } from '@google-cloud/firestore';
 import { toBool, toInt } from 'lib';
 
-const options: Settings = {
-  projectId: process.env.FIRESTORE_PROJECT_ID || 'local',
-  ssl: toBool(process.env.FIRESTORE_SSL),
-};
-
-if (process.env.FIRESTORE_HOST) {
-  options.host = process.env.FIRESTORE_HOST;
-}
-
-if (process.env.FIRESTORE_PORT) {
-  options.port = toInt(process.env.FIRESTORE_PORT);
-}
+// in production, use the values provided by cloud run context
+const options: Settings =
+  process.env.NODE_ENV !== 'production'
+    ? {
+        projectId: process.env.FIRESTORE_PROJECT_ID || 'local',
+        host: process.env.FIRESTORE_HOST,
+        port: toInt(process.env.FIRESTORE_PORT),
+        ssl: toBool(process.env.FIRESTORE_SSL),
+      }
+    : {};
 
 export const db = new Firestore(options);
