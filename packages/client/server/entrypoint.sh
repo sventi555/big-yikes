fb969cf77d72ebd741b29b306421b8c48e0a89c0
Stephen Edwards <34760298+sventi555@users.noreply.github.com>
2023-02-19 19:41:50 -0500

    fix: update client entrypoint env

--- packages/client/server/entrypoint.sh
+++ packages/client/server/entrypoint.sh
@@ -2,7 +2,14 @@
 
 # Construct an up to date environment.js file
 echo "window.ENV = {
-  \"apiHost\": \"$VITE_API_HOST\",
+  \"auth\": {
+    \"auth0Audience\": \"$VITE_AUTH0_AUDIENCE\",
+    \"auth0ClientId\": \"$VITE_AUTH0_CLIENT_ID\",
+    \"auth0Domain\": \"$VITE_AUTH0_DOMAIN\"
+  },
+  \"hosts\": {
+    \"api\": \"$VITE_API_HOST\";
+  }
 };" > /usr/share/nginx/html/environment.js;
 
 envsubst '${PORT}' < /etc/nginx/templates/default.conf.template > /etc/nginx/conf.d/default.conf
